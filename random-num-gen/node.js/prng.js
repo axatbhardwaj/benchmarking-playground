e9675a85582ade66f3ffa9e05e663c0f3c68b920
axatbhardwaj <axatbhardwaj@outlook.com>
2023-10-20 16:18:50 +0530

    rng node done

--- random-num-gen/node.js/prng.js
+++ random-num-gen/node.js/prng.js
@@ -1,36 +1,26 @@
-//taking salt from the system time
 function getSalt() {
-    let salt = Date.now() * new Date().getMilliseconds() * new Date().getSeconds() / (new Date().getHours());
-    salt = parseInt(salt);
-    //console.log(salt);
-    return salt;
+    const currentTime = new Date();
+    let salt = Date.now() * currentTime.getMilliseconds() * currentTime.getSeconds() / (currentTime.getHours() >> 0); // >> to make sure the no. is always positive
+    return salt | 0; // | to convert to int
 }
 
 function stringReverse(str) {
-    let str1 = str.split("").slice(0, str.length / 2);
-    let str2 = str.split("").slice(str.length / 2, str.length);
-    str1 = str1.reverse();
-    str2 = str2.reverse();
-    return str1.join("") + str2.join("")
+    const strArray = str.split("");
+    const str1 = strArray.slice(0, strArray.length / 2).reverse();
+    const str2 = strArray.slice(strArray.length / 2).reverse();
+    return str1.join("") + str2.join("");
 }
 
-//function to create the random number without using any library or inbuilt function
 function randNum(len) {
     len = parseInt(len);
-    let rNum = 0;
+    let rNum;
     do {
-        let salt = getSalt();
-        let salt2 = getSalt();
-        salt = stringReverse(salt.toString());
-        salt2 = stringReverse(salt2.toString());
-        salt = parseInt(salt);
-        // console.log(salt);
-        salt2 = parseInt(salt2);
-        // console.log(salt2);
-        rNum = salt + salt2;
-        rNum = rNum.toString();
-        rNum = rNum.slice(0, len);
-        //console.log(rNum);
+        const salt = getSalt();
+        const salt2 = getSalt();
+        const reversedSalt = stringReverse(salt.toString());
+        const reversedSalt2 = stringReverse(salt2.toString());
+        const sum = (parseInt(reversedSalt) | 0) + (parseInt(reversedSalt2) | 0);
+        rNum = sum.toString().slice(0, len);
     } while (rNum.toString().length !== len);
     return rNum;
 }
